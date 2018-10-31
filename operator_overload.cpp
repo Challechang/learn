b6ac170607993fc5226156482d6da36162010168
rbcheng <rbcheng@stu.edu.cn>
2018-10-31 11:52:18 +0800

    fix bugs in operator ++

--- operator_overload.cpp
+++ operator_overload.cpp
@@ -19,25 +19,44 @@ public:
     }
 
     // fetch and increment
-    INT& operator++(int) {
+    INT operator++(int) {
         INT temp = *this;
         ++(*this);
         return temp;
     }
 
-    INT& operator+(const INT& other) {
+    INT operator+(const INT& other) {
         INT temp(this->m_i + other.m_i);
         return temp;
     }
 
-    friend std::ostream& operator << (std::ostream& out, INT& anInt) {
+    INT operator-(const INT& other) {
+        INT temp(this->m_i - other.m_i);
+        return temp;
+    }
+
+
+
+    friend std::ostream& operator << (std::ostream& out, const INT& anInt) {
         out << anInt.m_i;
         return out;
     }
+
+    friend INT operator*(const INT& first, const INT& second);
 };
+
+INT operator*(const INT& first, const INT& second) {
+    INT temp(first.m_i * second.m_i);
+    return temp;
+}
 
 int main() {
     INT anInt(4), anInt1(5);
     std::cout << ++anInt << std::endl;
     std::cout << anInt++ << std::endl;
+    INT sum = anInt1 + anInt;
+    std::cout << anInt * anInt1 << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << anInt << std::endl;
+    std::cout << anInt1 << std::endl;
 }
