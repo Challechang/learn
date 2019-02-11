bda18e8cf5250a5cea85983f59d7f7a3a34a3c38
Challechang <794534487@qq.com>
2019-02-11 14:58:35 +0800

    更正对decltype的测试

--- decltype.cpp
+++ decltype.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <set>
 
 template <typename T1, typename T2>
 auto add(T1 t1, T2 t2)-> decltype(t1 + t2) {
@@ -23,6 +24,39 @@ void test_decltype(T obj) {
     i_type first = obj.begin();
     std::cout << *first << std::endl;
 }
+
+class Person {
+
+private:
+    std::string m_first_name;
+    std::string m_last_name;
+public:
+
+    friend std::ostream& operator<<(std::ostream& out, const Person& person) {
+        out << person.m_first_name << " " << person.m_last_name;
+        return out;
+    }
+
+    Person(const std::string& first_name, const std::string& last_name): m_first_name(first_name), m_last_name(last_name) {
+
+    }
+
+    void first_name(const std::string& first_name) {
+        m_first_name = first_name;
+    }
+
+    std::string first_name() const {
+        return m_first_name;
+    }
+
+    void last_name(const std::string& last_name) {
+            m_last_name = last_name;
+    }
+
+    std::string last_name() const {
+        return m_last_name;
+    }
+};
 
 int main() {
     /**
@@ -43,15 +77,18 @@ int main() {
     /**
      * Situation 3
      */
-    std::cout << "Situation 3: " << std::endl;
-    auto my_func = [](int i)->void {
-        std::cout << "my _func: " << i << std::endl;
+
+    auto cmp = [](const Person& p1, const Person& p2)-> bool {
+        return (p1.last_name() < p2.last_name()) || (p1.last_name() == p2.last_name() && p1.first_name() < p2.first_name());
     };
 
-    std::unordered_map<int, decltype(my_func)> m_map;
-    m_map.insert({1, my_func});
-    m_map.insert({2, my_func});
-    for (auto& _m_map: m_map) {
-        _m_map.second(_m_map.first);
+    std::cout << "Situation 3: " << std::endl;
+    std::set<Person, decltype(cmp)> persons(cmp);
+    persons.insert({"kobe", "brayant"});
+    persons.insert({"james", "jordan"});
+    persons.insert({"kevin", "duran"});
+    for (auto& person: persons) {
+        std::cout << person << std::endl;
     }
+
 }
